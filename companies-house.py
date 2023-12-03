0a5f2272c75f029412652c74c96734be6adbae1c
Paul Matthews <paul2.matthews@uwe.ac.uk>
2023-12-03 15:59:16 +0000

    bit more

--- companies-house.py
+++ companies-house.py
@@ -3,16 +3,21 @@ from requests.auth import HTTPBasicAuth
 import json
 import io
 
+# Get the API Key from the file
 api_key = open("ch-api-key.secret").readline()
 
+# This is the API "Endpoint"
 api_url = "https://api.company-information.service.gov.uk/advanced-search/companies"
 
+# The search parameters
 search_parms = "?size=10&location=bristol&company_name_includes=data&company_status=active&sic_codes=62020"
 
+# Adds the key to the request and make the call
 auth = HTTPBasicAuth(api_key, '')
-
 response = requests.get(api_url + search_parms, auth=auth)
 
-json_obj = json.loads(response.text)
+print(f'Return Code - {response.status_code}\n\n')
 
+# Pretty print the returned JSON data
+json_obj = json.loads(response.text)
 print(json.dumps(json_obj, indent=3))
--- scraping.py
+++ scraping.py
@@ -1,7 +1,6 @@
 import pyquery as pq
 import pandas as pd
 
-
 outputCards = []
 
 # Create a parsed DOM tree
@@ -10,14 +9,18 @@ parsed = pq.PyQuery(filename="scraped-html/startups.html", encoding='latin1')
 # Find the divs containing each company
 startupCards = parsed(".list-card")
 
+# Loop through each company and create a dictionary of the features
 for startupCard in startupCards:
     outputCard = dict()
     parsedCard = pq.PyQuery(startupCard)
     outputCard["title"] = parsedCard(".profile-info-left strong").text()
     outputCard["description"] = parsedCard(".left p").text()
-    #outputCard["funding"] = parsedCard(".right ul:first-child p").text()
+    outputCard["funding"] = parsedCard(".right ul li:nth-child(1) p").text()
     outputCards.append(outputCard)
 
+# Convert to Data Frame
 df = pd.DataFrame(outputCards)
+
+# Create CSV
 df.to_csv("data/startups.csv")
 print(df)
